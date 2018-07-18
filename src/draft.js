3cc1178c5ee1fe8e33991fdbc9e1f851702f1adb
cw0102 <cw0102@users.noreply.github.com>
2018-07-18 17:26:53 -0400

    DraftBot

---
+++ src/draft.js
@@ -0,0 +1,17 @@
+class Draft {
+    constructor() {
+
+    }
+}
+
+export class BoosterDraft extends Draft {
+    
+}
+
+export class SealedDraft extends Draft {
+    
+}
+
+export class CubeDraft extends Draft {
+    
+}
